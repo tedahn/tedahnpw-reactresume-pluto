c4b6443048ec743c6d8568193bcdd040ae142c24
Ted <tahn1996@gmail.com>
2021-07-19 02:26:39 -0500

    Fixing some CSS issues

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -8,62 +8,64 @@ class Work extends Component {
   render() {
     function desktopCards(portfolio, icons){
       return (
-        <Fade right delay={delayUp}>
-          <a href={portfolio.url}>
-            <card>
-              <div className="card-image">
-                <img src={portfolio.image} alt="Project Picture" />
-              </div>
-              <div className="card-content">
-                <h1>{portfolio.title}</h1>
-                <span>{portfolio.dates}</span>
-                <p>{portfolio.description}</p>
-                
-                <div className="tools">
-                    {icons}
-                </div>
-              </div>
-            </card>
-          </a>
-        </Fade>)
+              <card>
+                <a href={portfolio.url}>
+                  <div>
+                    <div className="card-image">
+                      <img src={portfolio.image} alt="Project Picture" />
+                    </div>
+                    <div className="card-content">
+                      <h1>{portfolio.title}</h1>
+                      <span>{portfolio.dates}</span>
+                      <p>{portfolio.description}</p>
+                      
+                      <div className="tools">
+                          {icons}
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </card>
+      )
     }
 
     function mobileCards(portfolio, icons){
       return (
-        <Fade right delay={delayUp}>
-          <a href={portfolio.url}>
-            <card>
-              <div className="card-image">
-                <img src={portfolio.image} alt="Project Picture" />
-              </div>
-              <div className="card-content">
-                <h1>{portfolio.title}</h1>
-                <span>{portfolio.dates}</span>
-                <p>{portfolio.description}</p>
-                
-                <div className="tools">
-                    {icons}
+          <div>
+            <a href={portfolio.url}>
+              <card>
+                <div className="card-image">
+                  <img src={portfolio.image} alt="Project Picture" />
                 </div>
-              </div>
-            </card>
-          </a>
-        </Fade>)
+                <div className="card-content">
+                  <h1>{portfolio.title}</h1>
+                  <span>{portfolio.dates}</span>
+                  <p>{portfolio.description}</p>
+                  
+                  <div className="tools">
+                      {icons}
+                  </div>
+                </div>
+              </card>
+            </a>
+          </div>
+      )
     }
 
     if(this.props.data){
       var delayUp = 400;
-      var projectList = this.props.data.projects.map(function(portfolio){
-        var icons = portfolio.tools.map(function(tool){
+      var projectList = this.props.data.projects.map(portfolio => {
+        const icons = portfolio.tools.map(tool => {
           let name = ["fab", tool];
           return <FontAwesomeIcon icon={name}/>
         })
         delayUp += 300;
         return (
           <div>
-            <MediaQuery minWidth={950}>
+            <MediaQuery minWidth={250}>
               {desktopCards(portfolio, icons)}
             </MediaQuery>
-            <MediaQuery maxWidth={950}>
+            <MediaQuery maxWidth={250}>
               {mobileCards(portfolio, icons)}
             </MediaQuery>
           </div>
@@ -76,9 +78,11 @@ class Work extends Component {
           <Fade bottom delay={300}>
           <h1>My Projects and Works</h1>
           </Fade>
-          <div className="container">
-            {projectList}
-          </div>
+          <Fade right delay={delayUp}>
+            <div className="container">
+              {projectList}
+            </div>
+          </Fade>
         </work>
     )
   }
