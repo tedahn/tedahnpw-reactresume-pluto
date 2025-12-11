8c4dd47414fa597e90ebfe3ad3ac39a2322eb43e
Ted Ahn <tedahn@mac.lan>
2025-12-11 03:16:59 -0600

    fix: restore missing library import in icons.js

--- src/icons.js
+++ src/icons.js
@@ -1,3 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin, faInstagram, faReact, faPython, faJs, faCss3 } from '@fortawesome/free-brands-svg-icons';
 import { faChartLine, faCode, faDatabase, faEnvelope, faPaperPlane, faMapMarkerAlt, faPhone, faQuoteLeft, faQuoteRight, faBrain, faRobot, faEye } from '@fortawesome/free-solid-svg-icons';
 
